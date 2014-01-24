823cad24780d90818f8c664a0b8e26ac0103a7a1
Grimcode <tti13stuukkak@kamk.fi>
2014-01-24 18:20:50 +0200

    Initial Commit
    Base.

---
+++ Game/Main.cpp
@@ -0,0 +1,31 @@
+#include <Windows.h>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <map>
+#include <utility>
+#include <string>
+#include <vector>
+using namespace std;
+using namespace sf;
+
+RenderWindow window; // Window that serves as a target for OpenGL rendering.
+Event event; // Event processing.
+
+int main()
+{
+	window.create(VideoMode(1024, 576), "My window"); 
+
+	while (window.isOpen()) 
+	{
+	   while (window.pollEvent(event)) 
+			{
+			   if (event.type == Event::Closed) 
+				   window.close();
+			}
+	   window.clear(Color::Blue); 
+	   window.display(); 
+	}
+
+	//system("PAUSE");
+    return 0;
+}
