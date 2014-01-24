c389a1d2ace59cc96a8c949e059aa276a8f3faf0
Grimcode <tti13stuukkak@kamk.fi>
2014-01-24 18:44:31 +0200

    Ekat Filut
    Reaktiopeli.

---
+++ Game/ReactionGame.cpp
@@ -0,0 +1,5 @@
+#include "ReactionGame.h"
+
+void game()
+{
+}
---
+++ Game/ReactionGame.h
@@ -0,0 +1,12 @@
+// FGJ14 on paras, ja pizza :---dDDDD
+#include <Windows.h>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <map>
+#include <utility>
+#include <string>
+#include <vector>
+using namespace std;
+using namespace sf;
+
+void game();
